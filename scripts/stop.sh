a1f97c5ae90cd130f35e9b1af781f99f1ec590f6
Dev-KUN-F <k3614900@gmail.com>
2023-12-18 19:22:47 +0900

    start.sh와 stop.sh 에 echo 추가

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,12 +1,16 @@
 cd /home/ec2-user/apps/imad-web
-var=$(ps -ef | grep yarn)
-pid=$(echo ${var} | cut -d " " -f2)
+# var=$(ps -ef | grep yarn)
+# pid=$(echo ${var} | cut -d " " -f2)
  
-if [ -n "${pid}" ]
-then
-    kill -9 ${pid}
-    echo $* is terminated.
+# if [ -n "${pid}" ]
+# then
+#     kill -9 ${pid}
+#     echo $* is terminated.
  
-else
-    echo $* is not running.
-fi
+# else
+#     echo $* is not running.
+# fi
+
+fuser -k 3000/tcp
+
+echo 서버종료
