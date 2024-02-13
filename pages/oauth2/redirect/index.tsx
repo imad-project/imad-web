a848b1a1ed90267a7e3bc4dea5580ef7f8de1d95
Dev-KUN-F <k3614900@gmail.com>
2024-02-14 01:39:36 +0900

    get요청으로변경

--- pages/oauth2/redirect/index.tsx
+++ pages/oauth2/redirect/index.tsx
@@ -9,7 +9,7 @@ export default function KakaoRedirect(): JSX.Element {
 
   const PATCHUSER = async () => {
     await axios
-      .patch("https://ncookie.site/api/user", {
+      .get("https://ncookie.site/api/user", {
         headers: {
           Authorization: `Bearer ${getCookie("Authorization")}`,
         },
