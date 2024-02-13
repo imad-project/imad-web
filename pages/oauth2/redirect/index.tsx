33e2eed28e7c5dc39771c45a2c26d3d8aa52fbf6
Dev-KUN-F <k3614900@gmail.com>
2024-02-14 01:06:41 +0900

    소셜로그인 리디렉션페이지에서 PATCHUSER 실행
    - 분기처리이후 유저정보수정페이지나 로그인성공페이지로 리디렉션

--- pages/oauth2/redirect/index.tsx
+++ pages/oauth2/redirect/index.tsx
@@ -1,18 +1,53 @@
 import { useRouter } from "next/router";
-import { setCookie } from "../../../src/commons/cookies/cookie";
+import { setCookie, getCookie } from "../../../src/commons/cookies/cookie";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import axios from "axios";
 
 export default function KakaoRedirect(): JSX.Element {
   const params = useParams();
   const router = useRouter();
+
+  const PATCHUSER = async () => {
+    await axios
+      .patch("https://api.iimad.com/api/user", {
+        headers: {
+          Authorization: `Bearer ${getCookie("Authorization")}`,
+        },
+      })
+      .then((res) => {
+        if (res.status === 200) {
+          console.log(res.data);
+          const accessToken = res.headers["authorization"];
+          setCookie("Authorization", accessToken, {
+            path: "/",
+            secure: true,
+            sameSite: "none",
+          });
+          if (res.data.data.role === "GUEST") {
+            console.log(res.data.data.role);
+            router.push("/signup/SetUserData");
+          } else if (res.data.data.role === "USER") {
+            console.log(res.data.data.role);
+            router.push("/login/success");
+          }
+        }
+
+        return res.data;
+      })
+      .catch((res) => {
+        alert(res?.response.data.message);
+        return;
+      });
+  };
+
   useEffect(() => {
     setCookie("Authorization", params.token, {
       path: "/",
       secure: true,
       sameSite: "none",
     });
-    router.push("/login/success");
+    PATCHUSER();
   }, []);
 
   return (
