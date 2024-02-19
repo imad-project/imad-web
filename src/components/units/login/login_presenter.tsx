46089d6c81c87ea45ab45e749f6504389c3d9401
Dev-KUN-F <k3614900@gmail.com>
2024-02-19 22:49:13 +0900

    로그인 페이지에 일반회원가입 버튼 추가

--- src/components/units/login/login_presenter.tsx
+++ src/components/units/login/login_presenter.tsx
@@ -7,6 +7,7 @@ export default function LoginPageUI(props: any): JSX.Element {
       <div>비밀번호</div>
       <input type="password" onChange={props.onChangePassWord} />
       <button onClick={props.onClickLogin}>로그인</button>
+      <button onClick={props.onClickSignUp}>회원가입</button>
       <button onClick={props.onClickLoginKakao}>카카오 로그인</button>
       <button onClick={props.onClickLoginGoogle}>구글 로그인</button>
       <button onClick={props.onClickLoginNaver}>네이버 로그인</button>
