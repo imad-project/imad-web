dc11d363108c00d74605ccbd66837f873e7eb0c4
Dev-KUN-F <k3614900@gmail.com>
2024-07-03 23:45:35 +0900

    네비게이션바 로그인시 분할처리
    - 로그인을 하여 cookie 에 Authorization 토큰이 있을경우 로그인 버튼 대신 유저의 이름과  로그아웃 버튼을 나타나게 함
    
    - 유저이름을 클릭시 자신의 유저정보창 /user 로 이동시키고
    로그아웃 버튼을 누를시엔 쿠키에 있는 Authorization 토큰을 삭제후 메인 화면으로 재라우팅해 로그아웃을 시킴

--- src/commons/layout/navigation/navigation.presenter.tsx
+++ src/commons/layout/navigation/navigation.presenter.tsx
@@ -1,6 +1,7 @@
 import { Fragment } from "react";
 import * as S from "./navigation.styles";
 import { INavigationUIProps } from "./navigation.types";
+import { getCookie } from "../../cookies/cookie";
 const NAVIGATION_MENUS = [
   { name: "장르추천", page: "/genre" },
   { name: "리뷰 검색", page: "/review" },
@@ -21,9 +22,21 @@ export default function NavigationUI(props: INavigationUIProps): JSX.Element {
           </Fragment>
         ))}
       </S.ItemBox>
-      <S.MenuItem id="/login" onClick={props.onClickMenu}>
-        로그인
-      </S.MenuItem>
+      {getCookie("Authorization") ? (
+        <S.MenuItem id="/user" onClick={props.onClickMenu}>
+          {props.userData.nickname}
+        </S.MenuItem>
+      ) : null}
+
+      {getCookie("Authorization") ? (
+        <S.MenuItem id="/" onClick={props.onClickLogout}>
+          로그아웃
+        </S.MenuItem>
+      ) : (
+        <S.MenuItem id="/login" onClick={props.onClickMenu}>
+          로그인
+        </S.MenuItem>
+      )}
     </S.Wrapper>
   );
 }
