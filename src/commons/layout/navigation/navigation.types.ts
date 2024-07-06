b97a40720187fdcdd56fcdba2ac4889e5803bd01
Dev-KUN-F <k3614900@gmail.com>
2024-07-06 22:54:55 +0900

    #93 네비게이션바 유저닉네임 출력오류 2
    - 첫번째 방안으로 해결되지않아 기존의 userData 형식에서 기본값을 "" 에서  null로 바꿈
    
    - 기존 presneter에서 props.userData?.nickname으로 출력방안을 바꾸어 유저데이터가 들어왔을때만 출력을하도록 바꾸었다.

--- src/commons/layout/navigation/navigation.types.ts
+++ src/commons/layout/navigation/navigation.types.ts
@@ -1,18 +1,19 @@
 import type { MouseEvent } from "react";
+interface IUserData {
+  gender: string;
+  age_range: number;
+  profile_image: number;
+  nickname: string;
+  preferred_tv_genres: [];
+  preferred_movie_genres: [];
+  auth_provider: string;
+  role: string;
+  email: string;
+  birth_year: number;
+}
 
 export interface INavigationUIProps {
   onClickMenu: (event: MouseEvent<HTMLDivElement>) => void;
   onClickLogout: (event: MouseEvent<HTMLDivElement>) => void;
-  userData: {
-    gender: string;
-    age_range: number;
-    profile_image: number;
-    nickname: string;
-    preferred_tv_genres: [];
-    preferred_movie_genres: [];
-    auth_provider: string;
-    role: string;
-    email: string;
-    birth_year: number;
-  };
+  userData: IUserData | null;
 }
