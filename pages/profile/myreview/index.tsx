a7a40a43a5f9857f5225070b68f5236516d7cdab
Dev-KUN-F <k3614900@gmail.com>
2024-07-19 07:57:20 +0900

    #100 프로필 내 리뷰 페이지 통신부 작성
    - components - 프로필 하위 디렉토리에 myreview 디렉토리구조를 생성함
    
    - axios 통신부와 받아올 데이터의 타입추론을 작성함

---
+++ pages/profile/myreview/index.tsx
@@ -0,0 +1,1 @@
+export default function MyReview_Page() {}
---
+++ src/components/units/profile/myreview/myreview_presenter.tsx
@@ -0,0 +1,5 @@
+import { IMyReviewProps } from "./myreview_types";
+
+export default function MyReview_UI(props: IMyReviewProps) {
+  return <></>;
+}
