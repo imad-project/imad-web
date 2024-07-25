c43f597a035ae4cf3ea9bdc22a8db2f22f0e2cd8
Dev-KUN-F <k3614900@gmail.com>
2024-07-25 23:03:52 +0900

    #100  평점 도넛 그래프를 나타내기 위한 라이브러리 테스트
    - 평점 도넛그래프를 나타내기 위해 react-circular-progressbar 라이브러리를 설치하고 테스트함
    
    - 기존 설정값들을 변경하는데엔 성공했지만 애니메이션 적용을 고민중임

--- pages/test/index.tsx
+++ pages/test/index.tsx
@@ -1,19 +1,29 @@
-import { motion } from "framer-motion";
-import { useState } from "react";
+import styled from "@emotion/styled";
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
 
-const AnimatedComponent: React.FC = () => {
-  const [isToggled, setIsToggled] = useState(false);
+const VALUE = 30;
+const PATH_COLOR = "#5F81FF"; // 차트에 표시될 선
+const TRAIL_COLOR = "#DFE8FF"; // 차트의 배경선
+
+export default function CircularProgressChart() {
+  const ProgressbarContainer = styled.div`
+    width: 100px;
+    height: 100px;
+  `;
 
   return (
-    <div>
-      <button onClick={() => setIsToggled(!isToggled)}>Toggle</button>
-      <motion.div
-        animate={{ opacity: isToggled ? 1 : 0, x: isToggled ? 100 : 0 }}
-        transition={{ ease: "easeInOut", duration: 0.5 }}
-        style={{ width: 100, height: 100, backgroundColor: "blue" }}
+    <ProgressbarContainer>
+      <CircularProgressbar
+        value={30}
+        text={`${VALUE}%`}
+        className="progressbar"
+        strokeWidth={10}
+        styles={buildStyles({
+          pathColor: PATH_COLOR,
+          trailColor: TRAIL_COLOR,
+          textColor: "#2B2D36",
+        })}
       />
-    </div>
+    </ProgressbarContainer>
   );
-};
-
-export default AnimatedComponent;
+}
