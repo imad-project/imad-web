c40aefc94c02bf3eaa97aa015f1b45d6c20d6248
Dev-KUN-F <k3614900@gmail.com>
2024-08-02 18:10:08 +0900

    #100 내 게시물 페이지 styles 작성
    - styles 작성
    
    - 기존 내리뷰 페이지와 사이즈와 생김새는 비슷하지만 구성이 조금 달라질 예정

--- src/components/units/profile/mywrite/mywrite_presenter.tsx
+++ src/components/units/profile/mywrite/mywrite_presenter.tsx
@@ -1,5 +1,5 @@
-import { IMyWriteProps } from "./my_types";
-
+import { IMyWriteProps } from "./mywrite_types";
+import * as S from "./mywrite_styles";
 export default function MyWritePage_UI(props: IMyWriteProps) {
   return (
     <>
---
+++ src/components/units/profile/mywrite/mywrite_styles.ts
@@ -0,0 +1,28 @@
+import styled from "@emotion/styled";
+
+export const MainWrapper = styled.div`
+  width: 70%;
+  display: flex;
+  flex-direction: column;
+  margin-top: 10px;
+  border-radius: 10px;
+  padding: 20px;
+  box-shadow: 0px 0px 10px gray;
+`;
+
+export const writeWrapper = styled.div`
+  width: 100%;
+  height: auto;
+  /* height: 1847px; */
+  border: 1px solid black;
+  border-radius: 10px;
+  padding-left: 20px;
+  padding-top: 10px;
+  padding-right: 20px;
+  display: flex;
+  flex-direction: column;
+
+  border: none;
+  box-shadow: 0px 0px 10px gray;
+  margin-bottom: 10px;
+`;
