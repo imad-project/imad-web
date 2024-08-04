1d90824ebf2da730719accb88877d559c80ec8c9
Dev-KUN-F <k3614900@gmail.com>
2024-08-04 18:10:42 +0900

    #100 내 스크랩 페이지 구성
    - 스크랩은 게시물에만 작동하는 기능이기에 기본골자는 내게시물과 비슷하게 레이아웃을 구성함
    
    - 내 게시물과 비슷하게 레이아웃을 구성하였지만 스크랩을 가져오는 통신에는 내게시물 통신과 다르게 빠진 데이터가 몇개 있다. (좋아요/싫어요 cnt, 조회수 cnt, 등등)
    
    - 따라서 ios 의 내스크랩 페이지를 참고하여 없는데이터를 삭제하는 식으로 진행을 하였으며 파일구조를 작성하였다.

---
+++ src/components/units/profile/myscrap/myscrap_types.ts
@@ -0,0 +1,31 @@
+interface IScrapData {
+  details_list: [
+    {
+      posting_id: number;
+      contents_id: number;
+      contents_title: string;
+      contents_poster_path: string;
+      user_id: number;
+      user_nickname: string;
+      user_profile_image: string;
+      posting_title: string;
+      category: number;
+
+      created_date: string;
+
+      scrap_id: number | null;
+    }
+  ];
+  total_elements: number;
+  total_pages: number;
+  page_number: number;
+  number_of_elements: number;
+  size_of_page: number;
+  sort_direction: number;
+  sort_property: string;
+  search_type: any | null; // 'any'를 사용하여 어떤 형식도 올 수 있음을 표시, 타입이 특정되면 더 정확하게 작성 가능
+}
+
+export interface IMyScrapProps {
+  scrapData: IScrapData | null;
+}
