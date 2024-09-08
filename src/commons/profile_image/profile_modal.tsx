3a63ec5996b21ec0e991c0ea2a732f3d00c35a87
Dev-KUN-F <k3614900@gmail.com>
2024-09-08 17:13:38 +0900

    #110 프로필 변경 컴포넌트 모달창으로 불러오기
    - 간단한 모달창 컴포넌트에 프로필 변경 컴포넌트를 연결시켜 이후 간단하게 불러올수있도록 구성하였다.
    
    - 모달을 열기위한 boolean state 들만 있으면 onclick 이벤트로 간단하게 접근할수있도록 구성하였다.

---
+++ src/commons/profile_image/profile_modal.tsx
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import Modal from "react-modal";
+import ImageUpload from "./profile_image"; // ImageUpload 컴포넌트 불러오기
+import styled from "@emotion/styled";
+import { height } from "@mui/system";
+
+// 모달을 화면에 렌더링할 위치를 지정 (필수)
+
+// 커스텀 버튼 스타일
+const CustomButton = styled.button`
+  background-color: #008cba;
+  color: white;
+  padding: 12px 24px;
+  font-size: 16px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  &:hover {
+    background-color: #007bb5;
+  }
+`;
+
+// 모달 스타일 (react-modal의 인라인 스타일링)
+const customModalStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+    width: "70%",
+    height: "70%",
+    padding: "20px",
+    borderRadius: "10px",
+    boxShadow: "0px 4px 15px rgba(0, 0, 0, 0.2)",
+  },
+  overlay: {
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+};
+
+const Row_box = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export default function Profile_Modal(props: any) {
+  return (
+    <div>
+      <Modal
+        isOpen={props.isModalOpen}
+        onRequestClose={props.closeModal}
+        style={customModalStyles}
+        contentLabel="Image Upload Modal"
+      >
+        <Row_box>
+          <h2>프로필 이미지 변경</h2>
+          <CustomButton onClick={props.closeModal}>닫기</CustomButton>
+        </Row_box>
+        <ImageUpload /> {/* ImageUpload 컴포넌트 */}
+      </Modal>
+    </div>
+  );
+}
