dacdb159a92327d5702865cadbf97a389a26955b
Dev-KUN-F <k3614900@gmail.com>
2024-09-18 13:23:05 +0900

    #109  추천api all 의 타입추론
    - 추천 api/all 의 응답 데이터를 타입추론을 하였다 응답의 길이가 길지만 형태가 같은 데이터의 반복이기에 그룹타입문으로 형식화 하여 해결하였다.

--- src/components/units/main/main.container.tsx
+++ src/components/units/main/main.container.tsx
@@ -1,9 +1,55 @@
 import { useEffect, useState } from "react";
 import MainPageUI from "./main.presenter";
 import axios from "axios";
+import { getCookie } from "@/src/commons/cookies/cookie";
+
+interface Tv {
+  id: number;
+  name: string;
+  genre_ids: number[];
+  poster_path: string;
+  backdrop_path: string | null;
+}
+interface Movie {
+  id: number;
+  title: string;
+  genre_ids: number[];
+  poster_path: string;
+  backdrop_path: string | null;
+}
+interface TvGroup {
+  page: number;
+  total_pages: number;
+  total_results: number;
+  results: Tv[];
+  contents_id: null;
+}
+interface MovieGroup {
+  page: number;
+  total_pages: number;
+  total_results: number;
+  results: Movie[];
+  contents_id: null;
+}
+
+interface Recommend_data {
+  preferred_genre_recommendation_tv: TvGroup | null;
+  preferred_genre_recommendation_movie: MovieGroup | null;
+  user_activity_recommendation_tv: TvGroup | null;
+  user_activity_recommendation_movie: MovieGroup | null;
+  user_activity_recommendation_tv_animation: TvGroup | null;
+  user_activity_recommendation_movie_animation: MovieGroup | null;
+  popular_recommendation_tv: TvGroup;
+  popular_recommendation_movie: MovieGroup;
+  top_rated_recommendation_tv: TvGroup;
+  top_rated_recommendation_movie: MovieGroup;
+  trend_recommendation_tv: TvGroup;
+  trend_recommendation_movie: MovieGroup;
+}
 
 export default function MainContainer(): JSX.Element {
   const [month, setMonth] = useState();
+  const token = getCookie("Authorization");
 
   const monthRanking = async () => {
     try {
