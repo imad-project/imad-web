b7f489e9e4691508cbe10c047eedcb251084ef0e
Dev-KUN-F <k3614900@gmail.com>
2024-11-26 15:47:30 +0900

    #111 회원가입 페이지의 수정
    - 회원가입시 이메일을 중복확인할수있는 버튼추가
    
    - 중복확인 버튼클릭시 이메일의 형식등을 검사하고 형식에 맞지않으면 경고메시지 출력
    
    - 형식을 맞춘 이메일 입력후 중복확인 버튼 클릭시 이메일 중복검사 로직 실행 중복검사후 결과에 따라 각각 초록 빨강으로 확인메시지 출력
    
    - 비밀번호 확인 인풋을 추가
    
    - 회원가입시 비밀번호 인풋과 비밀번호 확인 인풋이 동일하지않으면 경고메시지 출력

--- src/components/units/signup/signup_container.tsx
+++ src/components/units/signup/signup_container.tsx
@@ -8,9 +8,17 @@ import { setCookie } from "../../../commons/cookies/cookie";
 export default function SignupContainer(): JSX.Element {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
+  const [password2, setPassword2] = useState("");
+  const [isChecked, setIsChecked] = useState<boolean>(false);
+  const [emailChecked, setEmailChecked] = useState<boolean>(false);
   const router = useRouter();
 
+  const emailRegEx =
+    /^[A-Za-z0-9]([-_.]?[A-Za-z0-9])*@[A-Za-z0-9]([-_.]?[A-Za-z0-9])*\.[A-Za-z]{2,3}$/;
+
+  const emailCheck = (username: string) => {
+    return emailRegEx.test(username); //형식에 맞을 경우, true 리턴
+  };
   const SIGNUPIMAD = async () => {
     await axios
       .post("https://api.iimad.com/api/signup", {
@@ -58,15 +66,62 @@ export default function SignupContainer(): JSX.Element {
       });
   };
 
+  const EMAILCHECK = async () => {
+    if (email === "") {
+      alert("이메일이 비어있습니다.");
+      return;
+    }
+
+    if (!emailCheck(email)) {
+      alert("이메일 형식이 맞지 않습니다.");
+      return;
+    }
+
+    try {
+      const response = await axios.post(
+        "https://api.iimad.com/api/user/validation/email",
+        {
+          info: email,
+        }
+      );
+
+      if (response.status === 200) {
+        setIsChecked(true);
+        setEmailChecked(response.data.data.validation);
+      }
+    } catch (error) {
+      console.error("Error occurred while searching:", error);
+    }
+  };
+
   const onChangeEmail = (event: ChangeEvent<HTMLInputElement>) => {
     setEmail(event.target.value);
+    setIsChecked(false);
   };
 
   const onChangePassWord = (event: ChangeEvent<HTMLInputElement>) => {
     setPassword(event.target.value);
   };
 
+  const onChangePassWord2 = (event: ChangeEvent<HTMLInputElement>) => {
+    setPassword2(event.target.value);
+  };
+
   const onClickSignUp = () => {
+    if (isChecked === false) {
+      alert("이메일 중복확인을 해주세요!");
+      return;
+    }
+
+    if (emailChecked === false) {
+      alert("중복된 이메일입니다.");
+      return;
+    }
+
+    if (password !== password2) {
+      alert("비밀번호가 일치하지 않습니다.");
+      return;
+    }
     if (email === "" || password === "") {
       alert("회원가입에 사용할 아이디와 비밀번호를 입력해주세요.");
       return;
@@ -78,7 +133,11 @@ export default function SignupContainer(): JSX.Element {
     <SignupUI
       onChangeEmail={onChangeEmail}
       onChangePassWord={onChangePassWord}
+      onChangePassWord2={onChangePassWord2}
       onClickSignUp={onClickSignUp}
+      isChecked={isChecked}
+      emailChecked={emailChecked}
+      EMAILCHECK={EMAILCHECK}
     />
   );
 }
